e6a7d5ebad480d84c8409102110f803ef0721e2d
Jamison Greeley <berwyn.codeweaver@gmail.com>
2013-07-22 16:22:30 -0600

    Generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codeweaver::Application.config.secret_key_base = 'e1ea998e3ae715b5246d0bfb245b08d69cfdc5fa485ee3221b2aaaf003103319fe35257cf84d247d10b57ec8842c79b921f88ab4a7b6375a06607299694b2850'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codeweaver::Application.config.session_store :cookie_store, key: '_codeweaver_session'
