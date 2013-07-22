b61df0ae37c779ffc4ca133b05fb5784654c0ef4
Jamison Greeley <berwyn.codeweaver@gmail.com>
2013-07-22 16:48:44 -0600

    Route to index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Codeweaver::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
